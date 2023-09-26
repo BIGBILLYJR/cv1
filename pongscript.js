499e72fb3c24fe91a9a6b5c13c9c3a255984e985
BIGBILLYJR <118491605+BIGBILLYJR@users.noreply.github.com>
2023-09-26 10:48:47 -0400

    Add files via upload

---
+++ pongscript.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let leftScore = 0;
+    let rightScore = 0;
+    
+    const ball = document.querySelector('.ball');
+    const leftPaddle = document.getElementById('leftPaddle');
+    const rightPaddle = document.getElementById('rightPaddle');
+    const winMessage = document.getElementById('winMessage'); // Added winMessage element
+
+    leftPaddle.style.top = '150px';
+    rightPaddle.style.top = '150px';
+
+    let ballX = 400;
+    let ballY = 200;
+    let ballSpeedX = 5;
+    let ballSpeedY = 2;
+
+    function moveBall() {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        let rightPaddleY = parseInt(rightPaddle.style.top);
+        let rightPaddleSpeed = 2;
+
+        if (ballY > rightPaddleY + 50) {
+            rightPaddleY += rightPaddleSpeed;
+        } else if (ballY < rightPaddleY + 50) {
+            rightPaddleY -= rightPaddleSpeed;
+        }
+
+        if (rightPaddleY < 0) rightPaddleY = 0;
+        if (rightPaddleY > 300) rightPaddleY = 300;
+
+        rightPaddle.style.top = rightPaddleY + 'px';
+
+        if (ballY < 0 || ballY > 380) {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (
+            ballX < 30 && ballY > leftPaddle.offsetTop && ballY < leftPaddle.offsetTop + 100 ||
+            ballX > 750 && ballY > rightPaddle.offsetTop && ballY < rightPaddle.offsetTop + 100
+        ) {
+            ballSpeedX = -ballSpeedX;
+        }
+
+        if (ballX < 0 || ballX > 800) {
+            if (ballX < 0) {
+                rightScore++;
+            } else {
+                leftScore++;
+            }
+
+            document.getElementById('leftScore').textContent = leftScore;
+            document.getElementById('rightScore').textContent = rightScore;
+
+            if (leftScore >= 10) {
+                winMessage.textContent = "You Win!";
+                winMessage.style.display = "block";
+                return;
+            }
+
+            if (rightScore >= 10) {
+                winMessage.textContent = "You Lose!";
+                winMessage.style.display = "block";
+                return;
+            }
+
+            ballX = 400;
+            ballY = 200;
+            ballSpeedX = -ballSpeedX;
+        }
+
+        ball.style.left = ballX + 'px';
+        ball.style.top = ballY + 'px';
+
+        requestAnimationFrame(moveBall);
+    }
+
+    document.addEventListener('mousemove', (e) => {
+        const newPaddleY = e.clientY - 50;
+
+        if(newPaddleY >= 0 && newPaddleY <= 300) {
+            leftPaddle.style.top = newPaddleY + 'px';
+        }
+    });
+
+    moveBall();
+});
